95ac00c85ea2d108b6559a249da3e313f29b2191
PizzaCrust <torchprojectserver@gmail.com>
2016-03-30 14:44:42 +0700

    IoL class

---
+++ src/main/java/net/pizzacrust/iol/IoL.java
@@ -0,0 +1,20 @@
+package net.pizzacrust.iol;
+
+import java.lang.instrument.Instrumentation;
+
+/**
+ * The entry point of the Javaagent of IoL.
+ *
+ * @since 1.0-SNAPSHOT
+ * @author PizzaCrust
+ */
+public class IoL {
+    /**
+     * The Javaagent entry method for IoL.
+     * @param agentArguments the arguments given to the agent
+     * @param instrumentation the instrumentation object of the class loader
+     */
+    public static void premain(String agentArguments, Instrumentation instrumentation) {
+
+    }
+}
